5ecdad312ed34a26fc441e5222d6f0733310da2d
jack-kower <kowerjack@gmail.com>
2025-06-25 12:27:49 -0700

    script to find if two slopes are perpindicular v2

--- perpindicular.py
+++ perpindicular.py
@@ -1,10 +1,13 @@
 # m1*m2=-1
 # m2 = -1/m1
 
-m1=2
-m2=-1/2
+m1=3/2
+m2=3/2
 
 if( m2 == -1/m1):
     print("YES! m2 is the negative reciprocal of m1, indicating perpendicular lines.")
+elif(m1 ==  m2):
+    print("The lines are parallel, not perpendicular.")
 else:
     print("NO! m2 is not the negative reciprocal of m1, indicating the lines are not perpendicular.")
+
